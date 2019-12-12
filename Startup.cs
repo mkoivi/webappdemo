db2b702aca22a790ad72b5790374e1f1642b8d38
mkoivi <marko.kooo@gmail.com>
2019-12-12 16:06:03 +0200

    next commit

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace webappdemo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello *!");
             });
         }
     }
